13fe384c4b68436833765bc3e6402f493851a91a
Van Thanh Tran <vantran9867@gmail.com>
2021-03-02 01:13:52 +1100

    Being fixed

--- plot_graph.py
+++ plot_graph.py
@@ -4,18 +4,46 @@ from matplotlib.dates import DateFormatter, MonthLocator
 import matplotlib.pyplot as plt
 
 with open('aus_covid.json') as json_file:
-    data = json.load(json_file)
-    x = []
-    y = []
+    data1 = json.load(json_file)
+    x1 = []
     y1 = []
-    for record in data:
+    y2 = []
+    y3 = []
+    y4 = []
+    y5 = []
+    y6 = []
+    y7 = []
+    y8 = []
+    for record in data1:
         if record['Province'] == 'Victoria':
             date = datetime.datetime.strptime(record['Date'], '%Y-%m-%dT%H:%M:%SZ')
-            x.append(date.date())
-            y.append(record['Cases'])
+            x1.append(date.date())
+            y1.append(record['Cases'])
+        elif record['Province'] == 'Western Australia':
+            y2.append(record['Cases'])
+        elif record['Province'] == 'Australian Capital Territory':
+            y3.append(record['Cases'])
+        elif record['Province'] == 'New South Wales':
+            y4.append(record['Cases'])
+        elif record['Province'] == 'Northern Territory':
+            y5.append(record['Cases'])
+        elif record['Province'] == 'Queensland':
+            y6.append(record['Cases'])
+        elif record['Province'] == 'South Australia':
+            y7.append(record['Cases'])
+        elif record['Province'] == 'Tasmania':
+            y8.append(record['Cases'])
 
-fig, ax = plt.subplots()
-ax.plot(x, y, label='Victoria')
+fig, (ax1, ax2) = plt.subplots()
+ax1.plot(x1, y1, label='Victoria')
+ax1.plot(x1, y2, label='Western Australia')
+ax1.plot(x1, y3, label='Australian Capital Territory')
+ax1.plot(x1, y4, label='New South Wales')
+ax1.plot(x1, y5, label='Northern Territory')
+ax1.plot(x1, y6, label='Queensland')
+ax1.plot(x1, y7, label='South Australia')
+ax1.plot(x1, y8, label='Tasmania')
+
 plt.xticks(rotation=45)
 plt.xlabel('Dates', fontsize=10)
 plt.ylabel('Cases', fontsize=10)
