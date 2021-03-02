2bf258235fda4ee900e0938eaf14da2fd4748202
Van Thanh Tran <vantran9867@gmail.com>
2021-03-03 08:56:08 +1100

    Reviewed

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ The graph was based on the daily COVID-19 cases recorded in Australia and
 Vietnam from 22/1/2020 to 22/2/2021.
 
 In this project, the learner practiced plotting graphs on matplotlib using the 
-JSON data. The "Date" format was researched and practiced.
+JSON data. The "Date" format was researched and practiced. 
 
-Pipnv was installed for the project. 
+#Instruction
+##Prerequisite
+-Python 3.8+
+-Pipenv
 
+##Install dependencies
+Run 'pipenv install'
+
+##Run plot graph
+Run 'python plot_graph.py'
--- plot_graph.py
+++ plot_graph.py
@@ -5,61 +5,61 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
-with open('aus_covid.json') as json_file:
-    data1 = json.load(json_file)
-    x1 = []
-    y1 = []
-    y2 = []
-    y3 = []
-    y4 = []
-    y5 = []
-    y6 = []
-    y7 = []
-    y8 = []
-    for record in data1:
+with open('aus_covid.json') as aus_file:
+    aus_data = json.load(aus_file)
+    date_aus = []
+    VIC = []
+    WA = []
+    ACT = []
+    NSW = []
+    NT = []
+    QLD = []
+    SA = []
+    TAS = []
+    for record in aus_data:
         if record['Province'] == 'Victoria':
             date = datetime.datetime.strptime(record['Date'], '%Y-%m-%dT%H:%M:%SZ')
-            x1.append(date.date())
-            y1.append(record['Cases'])
+            date_aus.append(date.date())
+            VIC.append(record['Cases'])
         elif record['Province'] == 'Western Australia':
-            y2.append(record['Cases'])
+            WA.append(record['Cases'])
         elif record['Province'] == 'Australian Capital Territory':
-            y3.append(record['Cases'])
+            ACT.append(record['Cases'])
         elif record['Province'] == 'New South Wales':
-            y4.append(record['Cases'])
+            NSW.append(record['Cases'])
         elif record['Province'] == 'Northern Territory':
-            y5.append(record['Cases'])
+            NT.append(record['Cases'])
         elif record['Province'] == 'Queensland':
-            y6.append(record['Cases'])
+            QLD.append(record['Cases'])
         elif record['Province'] == 'South Australia':
-            y7.append(record['Cases'])
+            SA.append(record['Cases'])
         elif record['Province'] == 'Tasmania':
-            y8.append(record['Cases'])
+            TAS.append(record['Cases'])
 
-with open('vietnam_covid.json') as json_file:
-    x2 = []
-    y = []
-    data2 = json.load(json_file)
+with open('vietnam_covid.json') as vn_file:
+    vn_date = []
+    VN = []
+    data2 = json.load(vn_file)
     for record in data2:
         date = datetime.datetime.strptime(record['Date'], '%Y-%m-%dT%H:%M:%SZ')
-        x2.append(date.date())
-        y.append(record['Cases'])
+        vn_date.append(date.date())
+        VN.append(record['Cases'])
 
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
 ax1.set_title("Australia's Covid-19 cases daily increase")
-ax1.plot(x1, y1, label='Victoria')
-ax1.plot(x1, y2, label='Western Australia')
-ax1.plot(x1, y3, label='Australian Capital Territory')
-ax1.plot(x1, y4, label='New South Wales')
-ax1.plot(x1, y5, label='Northern Territory')
-ax1.plot(x1, y6, label='Queensland')
-ax1.plot(x1, y7, label='South Australia')
-ax1.plot(x1, y8, label='Tasmania')
+ax1.plot(date_aus, VIC, label='Victoria')
+ax1.plot(date_aus, WA, label='Western Australia')
+ax1.plot(date_aus, ACT, label='Australian Capital Territory')
+ax1.plot(date_aus, NSW, label='New South Wales')
+ax1.plot(date_aus, NT, label='Northern Territory')
+ax1.plot(date_aus, QLD, label='Queensland')
+ax1.plot(date_aus, SA, label='South Australia')
+ax1.plot(date_aus, TAS, label='Tasmania')
 ax1.legend()
 
 ax2.set_title("Vietnam's Covid-19 cases daily increase")
-ax2.plot(x2, y, color='#444444', linestyle='--', label='Viet Nam')
+ax2.plot(vn_date, VN, color='#444444', linestyle='--', label='Viet Nam')
 ax2.legend()
 
 
