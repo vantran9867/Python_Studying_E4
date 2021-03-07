8c42d6db2a452f967419052f5164b25a5d250e65
Van Thanh Tran <vantran9867@gmail.com>
2021-03-07 20:51:32 +1100

    Updated

--- corona_data_service.py
+++ corona_data_service.py
@@ -6,34 +6,6 @@ class CoronaDataService:
     def __init__(self, data_file: str):
         self.country_data = json_utilities.open_json_file(data_file)
 
-    def get_total_cases_in_country(self, country: str) -> int:
-        total_cases = 0
-        for record in self.country_data:
-            if record['Country'] == country:
-                total_cases += record['Cases']
-        return total_cases
-
-    def get_total_cases_in_province(self, province: str) -> int:
-        total_cases = 0
-        for record in self.country_data:
-            if record['Province'] == province:
-                total_cases += record['Cases']
-        return total_cases
-
-    def get_cases_list_in_country(self, country: str) -> list:
-        cases_list = []
-        for record in self.country_data:
-            if record['Country'] == country:
-                cases_list.append(record['Cases'])
-        return cases_list
-
-    def get_cases_list_in_province(self, province: str) -> list:
-        cases_list = []
-        for record in self.country_data:
-            if record['Province'] == province:
-                cases_list.append(record['Cases'])
-        return cases_list
-
     def get_record_of_province(self, province: str) -> list:
         records = []
         for record in self.country_data:
@@ -41,12 +13,12 @@ class CoronaDataService:
                 records.append(record)
         return records
 
-    def date_list(self) -> list:
-        date_list = []
+    def get_record_of_country(self, country: str) -> list:
+        records = []
         for record in self.country_data:
-            date = datetime.datetime.strptime(record['Date'], '%Y-%m-%dT%H:%M:%SZ')
-            date_list.append(date.date())
-        return date_list
+            if record['Country'] == country:
+                records.append(record)
+        return records
 
 
 
--- plot_graph_2.py
+++ plot_graph_2.py
@@ -7,47 +7,64 @@ from corona_data_service import CoronaDataService
 plt.style.use('seaborn')
 
 
-def get_data(data_service: CoronaDataService, province):
+def get_data_province(data_service: CoronaDataService, province):
+    date_list = []
     province_data = data_service.get_record_of_province(province)
-    date = [i['Date'] for i in province_data]
+    for i in province_data:
+        date = datetime.datetime.strptime(i['Date'], '%Y-%m-%dT%H:%M:%SZ')
+        date_list.append(date)
     cases = [i['Cases'] for i in province_data]
-    return date, cases
+    return date_list, cases
+
+
+def get_data_country(data_service: CoronaDataService, country):
+    date_list = []
+    country_data = data_service.get_record_of_country(country)
+    for i in country_data:
+        date = datetime.datetime.strptime(i['Date'], '%Y-%m-%dT%H:%M:%SZ')
+        date_list.append(date)
+    cases = [i['Cases'] for i in country_data]
+    return date_list, cases
 
 
 aus_data = CoronaDataService('aus_covid.json')
 vn_data = CoronaDataService('vietnam_covid.json')
 
-vic = get_data(aus_data, 'Victoria')
-act = get_data(aus_data, 'WA')
-
+vic = get_data_province(aus_data, 'Victoria')
+wa = get_data_province(aus_data, 'West Australia')
+act = get_data_province(aus_data, 'Australian Capital Territory')
+nsw = get_data_province(aus_data, 'New South Wales')
+nt = get_data_province(aus_data, 'Northern Territory')
+qld = get_data_province(aus_data, 'Queensland')
+sa = get_data_province(aus_data, 'South Australia')
+tas = get_data_province(aus_data, 'Tasmania')
 
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
 ax1.set_title("Australia's Covid-19 cases daily increase")
 ax1.plot(vic[0], vic[1], label='Victoria')
-# ax1.plot(date_aus, WA, label='Western Australia')
-# ax1.plot(date_aus, ACT, label='Australian Capital Territory')
-# ax1.plot(date_aus, NSW, label='New South Wales')
-# ax1.plot(date_aus, NT, label='Northern Territory')
-# ax1.plot(date_aus, QLD, label='Queensland')
-# ax1.plot(date_aus, SA, label='South Australia')
-# ax1.plot(date_aus, TAS, label='Tasmania')
+ax1.plot(wa[0], wa[1], label='West Australia')
+ax1.plot(act[0], act[1], label='Australian Capital Territory')
+ax1.plot(nsw[0], nsw[1], label='Australian Capital Territory')
+ax1.plot(nt[0], nt[1], label='Northern Territory')
+ax1.plot(qld[0], qld[1], label='Queensland')
+ax1.plot(sa[0], sa[1], label='South Australia')
+ax1.plot(tas[0], tas[1], label='Tasmania')
+
 ax1.legend()
 
-# ax2.set_title("Vietnam's Covid-19 cases daily increase")
-# ax2.plot(vn_date, VN, color='#444444', linestyle='--', label='Viet Nam')
-# ax2.legend()
-
-
-# ax2.xaxis.set_tick_params(rotation=45)
+vn = get_data_country(vn_data, 'Viet Nam')
+ax2.set_title("VietNam's Covid-19 cases daily increase")
+ax2.plot(vn[0], vn[1], color='#444444', linestyle='--', label='VietNam')
+ax2.xaxis.set_tick_params(rotation=45)
 
 plt.xlabel('Dates')
 ax1.set_ylabel('Cases')
-# ax2.set_ylabel('Cases')
+ax2.set_ylabel('Cases')
 
-# months = MonthLocator(bymonthday=22)
-# monthsFmt = DateFormatter("%Y-%m-%d")
-# ax2.xaxis.set_major_locator(months)
-# ax2.xaxis.set_major_formatter(monthsFmt)
+months = MonthLocator(bymonthday=22)
+monthsFmt = DateFormatter("%Y-%m-%d")
+ax2.xaxis.set_major_locator(months)
+ax2.xaxis.set_major_formatter(monthsFmt)
 
 plt.show()
